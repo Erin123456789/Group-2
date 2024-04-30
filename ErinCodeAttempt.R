96c8f54c9cdadfded540ee93ad7a00dc97a0a358
Erin <eweidenhamer@unomaha.edu>
2024-04-30 16:33:32 -0500

    Update ErinCodeAttempt.R
    Check for "Newest Code Attempt" for a cleaned up version of this code. I think that will be the one worked on from here on out

--- ErinCodeAttempt.R
+++ ErinCodeAttempt.R
@@ -10,6 +10,7 @@ db <- dbConnect(RMariaDB::MariaDB(),
                 dbname='marian', host='34.135.251.192')
 
 #create a list of the column names
+table <- "PreSAT"
 database_columns <- dbGetQuery(db, "DESCRIBE marian.ACT")
 db_ACT <- database_columns$Field
 database_columns2 <- dbGetQuery(db, "DESCRIBE marian.PreACT")
@@ -19,13 +20,15 @@ db_PreACT89 <- database_columns3$Field
 database_columns4 <- dbGetQuery(db, "DESCRIBE marian.PreSAT")
 db_PreSAT <- database_columns4$Field
 
+dbDisconnect(db)
+
 #UI
 ui <- fluidPage (
   titlePanel("Standardized Testing Data"),
   sidebarLayout(
     sidebarPanel(
         card(
-          card_header("File input"),
+          card_header("Choose CSV File"),
           fileInput("file", label = NULL),
           actionButton("upload", label="Upload")
             ),
@@ -53,8 +56,9 @@ server <- function(input, output) {
   #data preview
   selected_file_data <- reactive({
     req(input$file)
-    df <- read.csv(input$file$datapath, stringsAsFactors = FALSE)
+    read.csv(input$file$datapath)
   })
+  
     # Render data preview table
   output$contents <- renderTable({
     req(selected_file_data())
@@ -71,22 +75,12 @@ server <- function(input, output) {
   observeEvent(input$radio, {
     if(input$radio == 1 ) {
       output$database_column_select_ui <- renderUI({
-        selectInput("database_column","Select Database Column:", choices = db_ACT)
-          ACT <- c("student_id","test_date","district","score","new_pred_act")
-        columnnames <- colnames(ACT)
-        output$column <- renderUI({
-          selectInput("columnnames", label = "Select Database Column:", choices = columnnames)
-        })
+        selectInput("columnnames", label = "Select Database Column:", choices = db_ACT)
       })
-    }
+      }
     if(input$radio == 2) {
       output$database_column_select_ui <- renderUI({
-        selectInput("database_column","Select Database Column:", choices = db_PreACT)
-        PreACT <- c("student_id","test_date","district","score","new_pred","pred_act")
-        columnnames <- colnames(PreACT)
-        output$column <- renderUI({
-          selectInput("columnnames", label = "Select Database Column:", choices = columnnames)
-        })
+        selectInput("columnnames", label = "Select Database Column:", choices = db_PreACT)
       })
       }
     if(input$radio == 3) {
@@ -101,29 +95,58 @@ server <- function(input, output) {
   
   #READ THE SELECTED FILE AND EXTRACT COLUMN NAMES
   #THEN, IN THE SAME EVENT, GENERATE A SELECT INPUT WIDGET FOR THE UPLOAD FILE SELECTION
+    #gives 1 database/user file
   {
-    observeEvent(input$file, {req(input$file)
-      df <- read.csv(input$file$datapath, stringsAsFactors = FALSE)
-      columnnames <- colnames(df)
-      output$column_select_ui <- renderUI({
-      selectInput("column_names", label = "Select Your Columns", choices = columnnames)})
-  })
+    #observeEvent(input$file, {req(input$file)
+     # df <- read.csv(input$file$datapath, stringsAsFactors = FALSE)
+      #columnnames <- colnames(df)
+      #output$column_select_ui <- renderUI({
+      #selectInput("column_names", label = "Select Your Columns", choices = columnnames)})
+  #})
   }
-  
   #UPLOAD DATA INTO THE DATABASE
+  #{
+   # insert_data <- function(data, db) {
+    #  dbWriteTable(db, "your_table", data, append = TRUE)
+    #}
+    #observeEvent(input$upload, {
+     # if(!is.null(input$file)) {
+      #  df <- read.csv(input$file$datapath, stringsAsFactors = FALSE)
+       # insert_data(df,db)
+       # output$upload_status <- renderText ({
+       #   "Data Uploaded Successfully!"})
+      #}
+    #})
+  #}
+  
+  #RETRY OF THE ABOVE COMMENTED OUT - FROM HINTS VIDEO
+  
+  #Reactive expression to generate select inputs for column mapping
+    #just gives 1 database option
   {
-    insert_data <- function(data, db) {
-      dbWriteTable(db, "your_table", data, append = TRUE)
-    }
-    observeEvent(input$upload, {
-      if(!is.null(input$file)) {
-        df <- read.csv(input$file$datapath, stringsAsFactors = FALSE)
-        insert_data(df,db)
-        output$upload_status <- renderText ({
-          "Data Uploaded Successfully!"})
-      }
-        })
-      }
+    data <- reactive({
+      req(input$file)
+      read.csv(input$file$datapath)
+    })
+    output$column_mapping <- renderUI({
+    req(data(), db_ACT)
+    lapply(db_ACT,function(column){
+      selectInput(column, column, names(data()))
+    })
+    })
+  }
+  column_mapping <- reactive({
+    req(data())
+    sapply(db_ACT, function(column) {
+      input[[column]]
+    })
+  })
+  
+  #Save data to MySQL database when save button is clicked
+  observeEvent(input$upload, {
+    req(data(), column_mapping(), db_ACT, )
+  })
+
     }
   
 
